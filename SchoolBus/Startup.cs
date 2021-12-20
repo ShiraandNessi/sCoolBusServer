0dc73eebb5813f4cfcbde0fd6e3d90f46f987769
Nessi Hutzler <213087448@PC5-3>
2021-12-20 18:20:19 +0200

    logger

--- SchoolBus/Startup.cs
+++ SchoolBus/Startup.cs
@@ -60,8 +60,9 @@ namespace SchoolBus
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+            logger.LogInformation("server is up!!:)");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
